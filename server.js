16ade8c881b7d2bd62250bc2f8d47fa7df291f0d
Dina <counting1121@yahoo.com>
2019-02-12 20:08:05 -0500

    path update

--- server.js
+++ server.js
@@ -40,11 +40,11 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "home.html"));
 });
 
-app.get("/add", function(req, res) {
+app.get("/tables", function(req, res) {
   res.sendFile(path.join(__dirname, "tables.html"));
 });
 
-app.get("/all", function(req, res) {
+app.get("/reserve", function(req, res) {
   res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
