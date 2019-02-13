52e53f9daddbbfa81f2dc31abb4474349bddaa04
Dina <counting1121@yahoo.com>
2019-02-12 19:24:27 -0500

    backend server file

---
+++ server.js
@@ -0,0 +1,74 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/all", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+// Displays all tables
+app.get("/api/tables", function(req, res) {
+  return res.json(tables);
+});
+
+// Displays a single character, or returns false
+app.get("/api/tables/:tables", function(req, res) {
+  var chosen = req.params.tables;
+
+  console.log(chosen);
+
+  for (var i = 0; i < tables.length; i++) {
+    if (chosen === tables[i].routeName) {
+      return res.json(tables[i]);
+    }
+  }
+
+  return res.json(false);
+});
+
+// Create New Characters - takes in JSON input
+app.post("/api/tables", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var newtable = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newtable.routeName = newtable.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newtable);
+
+  characters.push(newtable);
+
+  res.json(newtable);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
